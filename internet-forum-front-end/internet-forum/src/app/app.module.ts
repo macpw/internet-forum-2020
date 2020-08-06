7f7fe26c32c2ba8e2ecaf75c450b457602f25b75
Paweł Mac <pawel.mac@gmail.com>
2020-08-06 22:15:50 +0200

    [fe] Rearrange code.
    app.component.html    - removed placeholder.
    app.component.css     - added ':host {display: block;}'
    app.component.ts      - removed 'title' property.
    app.component.spce.ts - removed tests, the context has changed.
    A few more minor changes.

--- internet-forum-front-end/internet-forum/src/app/app.module.ts
+++ internet-forum-front-end/internet-forum/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+  ],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
