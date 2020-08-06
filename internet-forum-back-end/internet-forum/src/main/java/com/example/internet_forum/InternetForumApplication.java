fe80069c405a7e2d51529959c82b0bcf33c95d1e
Paweł Mac <pawel.mac@gmail.com>
2020-08-06 14:27:39 +0200

    Created Spring Boot project.
    Generated on https://start.spring.io/
        Project Maven Project
       Language Java
    Spring Boot 2.3.2
    
    Project Metadata:
           Group com.example
        Artifact internet-forum
            Name internet-forum
     Description Internet Forum
    Package name com.example.internet_forum
       Packaging Jar
            Java 11
    
    Dependencies:
    [WEB]
    • Spring Web [WEB]
    • Rest Repositories [WEB]
    • Spring Session [WEB]
    [SQL]
    • H2 Database [SQL]
    • Spring Data JPA [SQL]
    • MySQL Driver [SQL]

---
+++ internet-forum-back-end/internet-forum/src/main/java/com/example/internet_forum/InternetForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.internet_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetForumApplication.class, args);
+    }
+
+}
---
+++ internet-forum-back-end/internet-forum/src/test/java/com/example/internet_forum/InternetForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internet_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
